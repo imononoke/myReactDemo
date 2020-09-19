b3d93046358b0ad6b0c0977c755668075cf403cf
Isa <jiazheng.sun@dianrong.com>
2020-09-19 21:19:40 +0800

    Init my react project

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class App extends React.Component {
+
+    onAdd() {
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>hello, Isa</h1>
+                <p>how are u</p>
+                <button onClick={this.onAdd}>Add</button>
+            </div>
+        );
+    }
+}
