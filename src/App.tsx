054827a00266beb6326ed009ff76e4d2fb2782d2
Isa <jiazheng.sun@dianrong.com>
2020-09-20 21:29:36 +0800

    add eslint config

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ export default class App extends React.Component {
         return (
             <div>
                 <h1>hello, Isa</h1>
-                <p>how are u</p>
                 <button onClick={this.onAdd}>Add</button>
                 <p>{count}</p>
             </div>
